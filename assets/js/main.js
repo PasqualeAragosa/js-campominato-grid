9baeca056ecfbac498593fb6b86cd6166702a9fc
Pasquale Aragosa <aragosap@gmail.com>
2022-10-17 15:40:16 +0200

    add grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,18 @@ boxEl.insertAdjacentHTML('afterbegin', `<button class="my_btn">Create Pitch</but
 const buttonEl = document.querySelector('.my_btn');
 
 buttonEl.addEventListener('click', function() {
+    //Aggiungi un'istruzione per cui la griglia si resetti
+
     //Seleziono 'select'
     const selectEl = document.getElementById('lvl');
     //Acquisisco la difficoltà del livello
-    const difficult = Number(selectEl.options[selectEl.selectedIndex].value);
-    console.log(difficult);
+    const sizeLvl = Number(selectEl.options[selectEl.selectedIndex].value);
+    //console.log(sizeLvl);
+
+    const gridEL = document.querySelector('.grid');
+
+    for (let i = sizeLvl; i > 0; i--) {
+        gridEL.insertAdjacentHTML('afterbegin', `<div class="box">${i}</div>`);
+        
+    }
 });
